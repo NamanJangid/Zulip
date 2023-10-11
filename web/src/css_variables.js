d2b3e681e0194799454e16fa221f4d9d6754affa
N-Shar-ma <bablinaneh@gmail.com>
2023-10-11 13:50:13 +0530

    compose: Add breakpoints for moving compose control buttons to popover.
    Before introducing multiple new formatting buttons, we adjust to new
    breakpoints so the row of buttons never overflows / pushes off the Send
    button, even in long word languages like Russian.

--- web/src/css_variables.js
+++ web/src/css_variables.js
@@ -17,6 +17,10 @@ const ms = 320; // Mobile small
 // Breakpoints for middle column
 const mc = 849; // Middle column as wide as it appears after the `sm` breakpoint
 
+// Breakpoints for shifting some compose control buttons to popover
+const cb = 1390; // for when right sidebar is expanded
+const bc = 1150; // for when right sidebar is collapsed
+
 module.exports = {
     media_breakpoints: {
         xs_min: xs + "px",
@@ -24,7 +28,9 @@ module.exports = {
         md_min: md + "px",
         mc_min: mc + "px",
         lg_min: lg + "px",
+        bc_min: bc + "px",
         xl_min: xl + "px",
+        cb_min: cb + "px",
         ml_min: ml + "px",
         mm_min: mm + "px",
         ms_min: ms + "px",
